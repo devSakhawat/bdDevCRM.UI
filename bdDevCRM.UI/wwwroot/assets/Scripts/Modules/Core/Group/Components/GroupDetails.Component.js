011b737aed10e075fa9ca78ff23e9e433d9e9003
Sakhawat Hossain <sakhawat.azolutionse@outlook.com>
2025-11-11 17:30:08 +0600

    Step 3.2 done

--- bdDevCRM.UI/wwwroot/assets/Scripts/Modules/Core/Group/Components/GroupDetails.Component.js
+++ bdDevCRM.UI/wwwroot/assets/Scripts/Modules/Core/Group/Components/GroupDetails.Component.js
@@ -1,1 +1,227 @@
-﻿
+﻿/*=========================================================
+ * Group Details Component
+ * File: GroupDetails.Component.js
+ * Description: Group details/form wrapper component
+ * Author: devSakhawat
+ * Date: 2025-11-11
+=========================================================*/
+
+var GroupDetailsComponent = (function () {
+  'use strict';
+
+  /**
+   * Constructor
+   */
+  function GroupDetailsComponent(options) {
+    // Call parent constructor
+    BaseComponent.call(this, options);
+
+    this.controller = options.controller;
+    this.name = 'GroupDetailsComponent';
+
+    // Child components (will be set by GroupModule)
+    this.infoComponent = null;
+    this.permissionComponent = null;
+
+    // Selectors
+    this.selectors = {
+      tabStrip: '#tabstrip',
+      saveButton: '#btnSave',
+      clearButton: '#btnClearAll'
+    };
+
+    // Tab strip instance
+    this.tabStrip = null;
+  }
+
+  // Inherit from BaseComponent
+  GroupDetailsComponent.prototype = Object.create(BaseComponent.prototype);
+  GroupDetailsComponent.prototype.constructor = GroupDetailsComponent;
+
+  /**
+   * Before initialization
+   */
+  GroupDetailsComponent.prototype.beforeInit = function () {
+    App.Logger.debug('GroupDetailsComponent: beforeInit');
+
+    // Subscribe to events
+    this.subscribe('group:edit-requested', this.onEditRequested.bind(this));
+    this.subscribe('group:saved', this.onGroupSaved.bind(this));
+  };
+
+  /**
+   * Render component
+   */
+  GroupDetailsComponent.prototype.render = function () {
+    App.Logger.debug('GroupDetailsComponent: render');
+
+    // Initialize tab strip
+    this.initializeTabStrip();
+  };
+
+  /**
+   * Initialize Kendo TabStrip
+   */
+  GroupDetailsComponent.prototype.initializeTabStrip = function () {
+    $(this.selectors.tabStrip).kendoTabStrip({
+      select: function (e) {
+        App.Logger.debug('GroupDetailsComponent: Tab selected', e.item);
+      }
+    });
+
+    this.tabStrip = $(this.selectors.tabStrip).data("kendoTabStrip");
+
+    if (this.tabStrip) {
+      this.tabStrip.select(0); // Select first tab
+      App.Logger.info('GroupDetailsComponent: TabStrip initialized');
+    } else {
+      App.Logger.error('GroupDetailsComponent: Failed to initialize TabStrip');
+    }
+  };
+
+  /**
+   * Bind events
+   */
+  GroupDetailsComponent.prototype.bindEvents = function () {
+    var self = this;
+
+    // Save button click
+    $(this.selectors.saveButton).on('click', function (e) {
+      e.preventDefault();
+      self.onSaveClick();
+    });
+
+    // Clear button click
+    $(this.selectors.clearButton).on('click', function (e) {
+      e.preventDefault();
+      self.onClearClick();
+    });
+  };
+
+  /**
+   * Set child components
+   */
+  GroupDetailsComponent.prototype.setChildComponents = function (infoComponent, permissionComponent) {
+    this.infoComponent = infoComponent;
+    this.permissionComponent = permissionComponent;
+
+    App.Logger.debug('GroupDetailsComponent: Child components set');
+  };
+
+  /**
+   * Event: Save button clicked
+   */
+  GroupDetailsComponent.prototype.onSaveClick = async function () {
+    try {
+      App.Logger.debug('GroupDetailsComponent: Save clicked');
+
+      // Validate form
+      if (!this.validate()) {
+        return;
+      }
+
+      // Collect data from all components
+      var groupData = this.collectFormData();
+
+      App.Logger.debug('GroupDetailsComponent: Collected form data', groupData);
+
+      // Save via controller
+      if (this.controller) {
+        await this.controller.saveGroup(groupData);
+        ToastrMessage.showSuccess('Group saved successfully!');
+      }
+
+    } catch (error) {
+      App.Logger.error('GroupDetailsComponent: Save failed', error);
+      ToastrMessage.showError('Failed to save group');
+    }
+  };
+
+  /**
+   * Event: Clear button clicked
+   */
+  GroupDetailsComponent.prototype.onClearClick = function () {
+    App.Logger.debug('GroupDetailsComponent: Clear clicked');
+
+    // Update button text
+    $(this.selectors.saveButton).text('Save');
+
+    // Clear via controller
+    if (this.controller) {
+      this.controller.clearGroup();
+    }
+
+    // Switch to first tab
+    if (this.tabStrip) {
+      this.tabStrip.select(0);
+    }
+  };
+
+  /**
+   * Event: Edit requested
+   */
+  GroupDetailsComponent.prototype.onEditRequested = function (data) {
+    App.Logger.debug('GroupDetailsComponent: Edit requested', data);
+
+    // Update button text
+    $(this.selectors.saveButton).text('Update');
+
+    // Switch to first tab
+    if (this.tabStrip) {
+      this.tabStrip.select(0);
+    }
+  };
+
+  /**
+   * Event: Group saved
+   */
+  GroupDetailsComponent.prototype.onGroupSaved = function (data) {
+    App.Logger.info('GroupDetailsComponent: Group saved');
+
+    // If it was a create, clear the form
+    if (data.isCreate) {
+      this.onClearClick();
+    }
+  };
+
+  /**
+   * Validate form
+   */
+  GroupDetailsComponent.prototype.validate = function () {
+    // Validate info component
+    if (this.infoComponent && typeof this.infoComponent.validate === 'function') {
+      if (!this.infoComponent.validate()) {
+        // Switch to info tab
+        if (this.tabStrip) {
+          this.tabStrip.select(0);
+        }
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  /**
+   * Collect form data from all components
+   */
+  GroupDetailsComponent.prototype.collectFormData = function () {
+    var data = {};
+
+    // Get data from info component
+    if (this.infoComponent && typeof this.infoComponent.getFormData === 'function') {
+      var infoData = this.infoComponent.getFormData();
+      Object.assign(data, infoData);
+    }
+
+    // Get data from permission component (will add later)
+    // if (this.permissionComponent && typeof this.permissionComponent.getFormData === 'function') {
+    //     var permissionData = this.permissionComponent.getFormData();
+    //     Object.assign(data, permissionData);
+    // }
+
+    return data;
+  };
+
+  return GroupDetailsComponent;
+})();
--- bdDevCRM.UI/wwwroot/assets/Scripts/Modules/Core/Group/Group.Module.js
+++ bdDevCRM.UI/wwwroot/assets/Scripts/Modules/Core/Group/Group.Module.js
@@ -18,47 +18,54 @@ var GroupModule = (function () {
   var _config = {
     name: 'Group',
     version: '1.0.0',
-    dependencies: ['GroupService']
+    author: 'devSakhawat',
+    dependencies: []
   };
 
-  // Initialize module
-  function _init(options = {}) {
+  /**
+   * Initialize module
+   */
+  function _init(options) {
     if (_initialized) {
-      console.warn('Group module already initialized');
+      console.warn('GroupModule: Already initialized');
       return;
     }
 
-    App.Logger.info('Initializing Group module...');
+    options = options || {};
 
-    // Check dependencies
-    if (!_checkDependencies()) {
-      App.Logger.error('Group module dependencies not met');
-      return;
+    App.Logger.info('GroupModule: Initializing...');
+
+    try {
+      // Create controller
+      _controller = new GroupController(options);
+
+      // Initialize components
+      _initializeComponents();
+
+      // Setup module events
+      _setupEvents();
+
+      _initialized = true;
+
+      App.Logger.info('GroupModule: Initialized successfully');
+
+      // Emit event
+      if (App.EventBus) {
+        App.EventBus.emit('module:initialized', { name: _config.name });
+      }
+
+    } catch (error) {
+      App.Logger.error('GroupModule: Initialization failed', error);
+      throw error;
     }
-
-    // Get service from DI
-    const groupService = App.DI.resolve('GroupService');
-
-    // Create controller
-    _controller = new GroupController({
-      service: groupService
-    });
-
-    // Initialize components
-    _initializeComponents();
-
-    // Setup module events
-    _setupEvents();
-
-    _initialized = true;
-    App.Logger.info('Group module initialized successfully');
   }
 
-  function _checkDependencies() {
-    return _config.dependencies.every(dep => App.DI.has(dep));
-  }
-
+  /**
+   * Initialize components
+   */
   function _initializeComponents() {
+    App.Logger.debug('GroupModule: Initializing components...');
+
     // Initialize Summary Grid
     _components.summary = new GroupSummaryComponent({
       el: '#divSummary',
@@ -67,74 +74,78 @@ var GroupModule = (function () {
     });
     _components.summary.init();
 
-    // Initialize Details Form
+    // Initialize Details Container
     _components.details = new GroupDetailsComponent({
       el: '#divDetails',
       controller: _controller
     });
     _components.details.init();
 
-    // Initialize Info Component
+    // Initialize Group Info Component
     _components.info = new GroupInfoComponent({
       el: '#divGroupId',
       controller: _controller
     });
     _components.info.init();
 
-    // Initialize Permission Components
-    _components.modulePermission = new ModulePermissionComponent({
-      el: '#divGroupPermision',
-      controller: _controller
-    });
-    _components.modulePermission.init();
+    // Set child components in details
+    _components.details.setChildComponents(_components.info, null);
 
-    _components.menuPermission = new MenuPermissionComponent({
-      el: '#divMenuPermission',
-      controller: _controller
-    });
-    _components.menuPermission.init();
+    App.Logger.info('GroupModule: Components initialized');
   }
 
+  /**
+   * Setup module events
+   */
   function _setupEvents() {
-    // Listen to group events
+    App.Logger.debug('GroupModule: Setting up events...');
+
+    // Listen to application events
     App.EventBus.on('group:selected', _onGroupSelected);
     App.EventBus.on('group:saved', _onGroupSaved);
     App.EventBus.on('group:deleted', _onGroupDeleted);
+
+    App.Logger.debug('GroupModule: Events setup complete');
   }
 
+  /**
+   * Event: Group selected
+   */
   function _onGroupSelected(data) {
-    App.Logger.debug('Group selected:', data);
-    _controller.loadGroup(data.groupId);
+    App.Logger.debug('GroupModule: Group selected', data);
+
+    if (_controller) {
+      _controller.loadGroup(data.groupId);
+    }
   }
 
+  /**
+   * Event: Group saved
+   */
   function _onGroupSaved(data) {
-    App.Logger.debug('Group saved:', data);
-    ToastrMessage.showSuccess('Group saved successfully');
-
-    // Refresh grid
-    if (_components.summary) {
-      _components.summary.refreshGrid();
-    }
+    App.Logger.info('GroupModule: Group saved', data);
   }
 
+  /**
+   * Event: Group deleted
+   */
   function _onGroupDeleted(data) {
-    App.Logger.debug('Group deleted:', data);
-    ToastrMessage.showSuccess('Group deleted successfully');
-
-    // Refresh grid
-    if (_components.summary) {
-      _components.summary.refreshGrid();
-    }
+    App.Logger.info('GroupModule: Group deleted', data);
   }
 
-  // Destroy module
+  /**
+   * Destroy module
+   */
   function _destroy() {
-    if (!_initialized) return;
+    if (!_initialized) {
+      return;
+    }
 
-    App.Logger.info('Destroying Group module...');
+    App.Logger.info('GroupModule: Destroying...');
 
     // Destroy all components
-    Object.values(_components).forEach(component => {
+    Object.keys(_components).forEach(function (key) {
+      var component = _components[key];
       if (component && typeof component.destroy === 'function') {
         component.destroy();
       }
@@ -145,29 +156,55 @@ var GroupModule = (function () {
     App.EventBus.off('group:saved', _onGroupSaved);
     App.EventBus.off('group:deleted', _onGroupDeleted);
 
+    // Clear references
     _components = {};
     _controller = null;
     _initialized = false;
 
-    App.Logger.info('Group module destroyed');
+    App.Logger.info('GroupModule: Destroyed');
   }
 
   // Public API
   return {
+    /**
+     * Module configuration
+     */
+    config: _config,
+
+    /**
+     * Initialize module
+     */
     init: _init,
+
+    /**
+     * Destroy module
+     */
     destroy: _destroy,
 
+    /**
+     * Get component by name
+     */
     getComponent: function (name) {
-      return _components[name];
+      return _components[name] || null;
     },
 
+    /**
+     * Get controller
+     */
     getController: function () {
       return _controller;
     },
 
-    config: _config
+    /**
+     * Check if initialized
+     */
+    isInitialized: function () {
+      return _initialized;
+    }
   };
 })();
 
 // Register module with App
-App.registerModule('Group', GroupModule);
+if (typeof App !== 'undefined') {
+  App.registerModule('Group', GroupModule);
+}
